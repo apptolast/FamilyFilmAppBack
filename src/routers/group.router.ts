c4e62b1f6d9f2ad81ffa17bea5923624eba03e14
TuColegaDev <tucolegadev@gmail.com>
2023-09-19 21:49:09 +0200

    (Fix) - Users / Group endpoints

--- src/routers/group.router.ts
+++ src/routers/group.router.ts
@@ -8,9 +8,6 @@ const groupRouter = Router();
 groupRouter.route('/').get(rescue(groupController.getAll));
 groupRouter.route('/:id').get(rescue(groupController.getById));
 
-groupRouter.route('users/:userId/groups').get(rescue(groupController.getAllByUserId));
-groupRouter.route('users/:userId/groups/:id').get(rescue(groupController.getOneByUserId));
-
 groupRouter.route('/').post(rescue(groupController.create));
 
 groupRouter.route('/:id').put(rescue(groupController.update));
--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import rescue from 'express-rescue';
 import userController from '../controllers/user.controller';
+import groupController from '../controllers/group.controller';
 import authMiddleware from '../middlewares/auth';
 
 const userRouter = Router();
 
 userRouter.route('/:id').get(authMiddleware, rescue(userController.getById));
+
+userRouter.route('/:userId/groups').get(rescue(groupController.getAllByUserId));
+userRouter.route('/:userId/groups/:id').get(rescue(groupController.getOneByUserId));
 
 export default userRouter;
