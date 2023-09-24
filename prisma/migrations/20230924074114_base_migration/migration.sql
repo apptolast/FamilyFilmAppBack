f13e63bad4419616e6978f1847c399c9a8734a49
TuColegaDev <tucolegadev@gmail.com>
2023-09-24 09:44:20 +0200

    fix: Updated app models

---
+++ prisma/migrations/20230924074114_base_migration/migration.sql
@@ -0,0 +1,103 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "firebase_uuid" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'USER',
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Group" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Group_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "GroupUsers" (
+    "user_id" INTEGER NOT NULL,
+    "group_id" INTEGER NOT NULL,
+
+    CONSTRAINT "GroupUsers_pkey" PRIMARY KEY ("user_id","group_id")
+);
+
+-- CreateTable
+CREATE TABLE "WatchList" (
+    "group_id" INTEGER NOT NULL,
+    "movie_id" INTEGER NOT NULL,
+
+    CONSTRAINT "WatchList_pkey" PRIMARY KEY ("group_id","movie_id")
+);
+
+-- CreateTable
+CREATE TABLE "ViewList" (
+    "group_id" INTEGER NOT NULL,
+    "movie_id" INTEGER NOT NULL,
+
+    CONSTRAINT "ViewList_pkey" PRIMARY KEY ("group_id","movie_id")
+);
+
+-- CreateTable
+CREATE TABLE "Genre" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Genre_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "GenreMovie" (
+    "genre_id" INTEGER NOT NULL,
+    "movie_id" INTEGER NOT NULL,
+
+    CONSTRAINT "GenreMovie_pkey" PRIMARY KEY ("genre_id","movie_id")
+);
+
+-- CreateTable
+CREATE TABLE "Movie" (
+    "id" SERIAL NOT NULL,
+    "adult" BOOLEAN NOT NULL DEFAULT false,
+    "title" TEXT NOT NULL,
+    "genre_ids" INTEGER[],
+    "language" TEXT NOT NULL,
+    "synopsis" TEXT,
+    "image" TEXT,
+    "release_date" TEXT,
+    "vote_average" DOUBLE PRECISION NOT NULL,
+    "vote_count" INTEGER,
+
+    CONSTRAINT "Movie_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "GroupUsers" ADD CONSTRAINT "GroupUsers_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "GroupUsers" ADD CONSTRAINT "GroupUsers_group_id_fkey" FOREIGN KEY ("group_id") REFERENCES "Group"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "WatchList" ADD CONSTRAINT "WatchList_group_id_fkey" FOREIGN KEY ("group_id") REFERENCES "Group"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "WatchList" ADD CONSTRAINT "WatchList_movie_id_fkey" FOREIGN KEY ("movie_id") REFERENCES "Movie"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ViewList" ADD CONSTRAINT "ViewList_group_id_fkey" FOREIGN KEY ("group_id") REFERENCES "Group"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ViewList" ADD CONSTRAINT "ViewList_movie_id_fkey" FOREIGN KEY ("movie_id") REFERENCES "Movie"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "GenreMovie" ADD CONSTRAINT "GenreMovie_genre_id_fkey" FOREIGN KEY ("genre_id") REFERENCES "Genre"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "GenreMovie" ADD CONSTRAINT "GenreMovie_movie_id_fkey" FOREIGN KEY ("movie_id") REFERENCES "Movie"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
