4f327791b30a4e43f24a4b6771588d45e4913699
TuColegaDev <tucolegadev@gmail.com>
2023-10-14 21:20:40 +0200

    feat: List all users for Admin role

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,25 +1,36 @@
-import { NextFunction, Request, Response } from 'express';
-import { StatusCodes } from 'http-status-codes';
-import prisma from '../lib/prisma';
+import { NextFunction, Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import prisma from "../lib/prisma";
 
 class UserController {
   async getById(req: Request, res: Response, next: NextFunction) {
     const data = await prisma.user.findUnique({
       where: { id: Number(req.params.id) },
     });
-    
-    if (!data)
-    return next({ status: StatusCodes.NOT_FOUND, message: 'User not found' });
-  
-  res.status(StatusCodes.OK).json({ status: 'success', data });
-}
-async getAll(req: Request, res: Response, next: NextFunction) {
-    const data = await prisma.user.findMany();
 
     if (!data)
-      return next({ status: StatusCodes.NOT_FOUND, message: 'Users not found' });
+      return next({ status: StatusCodes.NOT_FOUND, message: "User not found" });
 
-    res.status(StatusCodes.OK).json({ status: 'success', data });
+    res.status(StatusCodes.OK).json({ status: "success", data });
+  }
+  async getAll(req: Request, res: Response, next: NextFunction) {
+    const query: Object =
+      res.locals?.payload?.role !== "admin"
+        ? {
+            where: {
+              id: res.locals.payload.id,
+            },
+          }
+        : {};
+    const data = await prisma.user.findMany(query);
+
+    if (!data)
+      return next({
+        status: StatusCodes.NOT_FOUND,
+        message: "Users not found",
+      });
+
+    res.status(StatusCodes.OK).json({ status: "success", data });
   }
 }
 
